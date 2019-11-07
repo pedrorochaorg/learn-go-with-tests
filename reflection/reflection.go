28f8ed199a8146678d12610cedc037475270a856
Pedro Rocha <pedro.rocha@mindera.com>
2019-11-07 14:54:59 +0000

    Refactoring the get value logic to have a cleaner separation of duties

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,13 +3,7 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(input string)) {
-	val := reflect.ValueOf(x)
-
-
-	// If the field is a pointer we need to extract the underlying value before we can use it as a regular field.
-	if val.Kind() == reflect.Ptr {
-		val = val.Elem()
-	}
+	val := getValue(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
@@ -21,4 +15,15 @@ func walk(x interface{}, fn func(input string)) {
 			walk(field.Interface(), fn)
 		}
 	}
+}
+
+func getValue(x interface{}) reflect.Value {
+	val := reflect.ValueOf(x)
+
+	// If the field is a pointer we need to extract the underlying value before we can use it as a regular field.
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+
+	return val
 }
